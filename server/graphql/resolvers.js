5048b97532e3f7165ba41a8fa975fe1e7757aa43
Yasser Ehab <56257332+Yasser-dev@users.noreply.github.com>
2021-05-29 22:51:11 +0800

    User registration and validation

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,3 +1,6 @@
+const bcrypt = require("bcryptjs");
+const { UserInputError } = require("apollo-server");
+
 const { User } = require("../models");
 
 module.exports = {
@@ -9,6 +12,53 @@ module.exports = {
         return users;
       } catch (err) {
         console.log(err);
+      }
+    },
+  },
+  Mutation: {
+    register: async (_, args) => {
+      let { username, email, password, confirmPassword } = args;
+      let errors = {};
+
+      try {
+        // Validate input
+        if (email.trim() === "") errors.email = "Email can not be empty";
+        if (username.trim() === "")
+          errors.username = "Username can not be empty";
+        if (password.trim() === "")
+          errors.password = "Password can not be empty";
+        if (confirmPassword.trim() === "")
+          errors.confirmPassword = "Confirm password can not be empty";
+
+        if (password !== confirmPassword)
+          errors.confirmPassword = "Passwords must match";
+
+        if (Object.keys(errors).length > 0) {
+          throw errors;
+        }
+
+        // Hash password
+        password = await bcrypt.hash(password, 6);
+
+        // Create user
+        const user = await User.create({
+          username,
+          email,
+          password,
+        });
+
+        // Return user
+        return user;
+      } catch (err) {
+        console.log(err);
+        if (err.name === "SequelizeUniqueConstraintError") {
+          err.errors.forEach(
+            (e) => (errors[e.path] = `${e.path} is already taken`)
+          );
+        } else if (err.name === "SequelizeValidationError") {
+          err.errors.forEach((e) => (errors[e.path] = e.message));
+        }
+        throw new UserInputError("Bad input", { errors });
       }
     },
   },
