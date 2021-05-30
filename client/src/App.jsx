788dafd8d9f5e94fb348821b29d28c57f4de8a57
Yasser Ehab <56257332+Yasser-dev@users.noreply.github.com>
2021-05-30 22:41:17 +0800

    Setup Apollo Client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+import ApolloProvider from "./ApolloProvider";
+
 import "./App.scss";
-import { Col, Container, Row } from "react-bootstrap";
 import Register from "./pages/Register";
 function App() {
   return (
-    <Container className="pt-5 d-flex align-items-center justify-content-center">
-      <Register />
-    </Container>
+    <ApolloProvider>
+      <Container className="pt-5 d-flex align-items-center justify-content-center">
+        <Register />
+      </Container>
+    </ApolloProvider>
   );
 }
 
