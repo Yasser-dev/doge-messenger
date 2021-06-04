4eeec0a2af9ab000a2b3215139cc9d2699b7d34b
Yasser Ehab <56257332+Yasser-dev@users.noreply.github.com>
2021-06-04 23:41:05 +0800

    home nav

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,10 +1,24 @@
-import React, { createContext, useContext, useReducer } from "react";
+import React, { createContext, useReducer, useContext } from "react";
+import jwtDecode from "jwt-decode";
 
 const AuthStateContext = createContext();
 const AuthDispatchContext = createContext();
 
+let user = null;
+const token = localStorage.getItem("token");
+if (token) {
+  const decodedToken = jwtDecode(token);
+  const expiresAt = new Date(decodedToken.exp * 1000);
+
+  if (new Date() > expiresAt) {
+    localStorage.removeItem("token");
+  } else {
+    user = decodedToken;
+  }
+} else console.log("No token found");
+
 const authReducer = (state, action) => {
-  switch (action) {
+  switch (action.type) {
     case "LOGIN":
       localStorage.setItem("token", action.payload.token);
       return {
@@ -12,20 +26,24 @@ const authReducer = (state, action) => {
         user: action.payload,
       };
     case "LOGOUT":
+      localStorage.removeItem("token");
       return {
         ...state,
         user: null,
       };
     default:
-      throw new Error(`Unkown action type ${action.type}`);
+      throw new Error(`Unknown action type: ${action.type}`);
   }
 };
 
 export const AuthProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(authReducer, { user: null });
+  const [state, dispatch] = useReducer(authReducer, { user });
+
   return (
     <AuthDispatchContext.Provider value={dispatch}>
-      <AuthStateContext value={state}>{children}</AuthStateContext>
+      <AuthStateContext.Provider value={state}>
+        {children}
+      </AuthStateContext.Provider>
     </AuthDispatchContext.Provider>
   );
 };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { Button, Col, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useAuthDispatch } from "../context/auth";
+const Home = ({ history }) => {
+  const dispatch = useAuthDispatch();
 
-const Home = () => {
-  return <div>Home</div>;
+  const logout = () => {
+    dispatch({ type: "LOGOUT" });
+    history.push("/login");
+  };
+
+  return (
+    <Row align="center" className="bg-white">
+      <Col>
+        <Link to="/login">
+          <Button variant="link-dark">Login</Button>
+        </Link>
+      </Col>
+      <Col>
+        <Link to="/register">
+          <Button variant="link-dark">Register</Button>
+        </Link>
+      </Col>
+      <Col>
+        <Button variant="link-dark" onClick={logout}>
+          Logout
+        </Button>
+      </Col>
+    </Row>
+  );
 };
 
 export default Home;
