f40c68362a61f683cfc00d8aed75c77047ba8469
Yasser Ehab <56257332+Yasser-dev@users.noreply.github.com>
2021-06-13 04:06:37 +0800

    get user latest message

--- server/graphql/resolvers/index.js
+++ server/graphql/resolvers/index.js
@@ -2,6 +2,9 @@ const userResolvers = require("./users");
 const messageResolvers = require("./messages");
 
 module.exports = {
+  User: {
+    createdAt: (parent) => parent.createdAt.toISOString(),
+  },
   Message: {
     createdAt: (parent) => parent.createdAt.toISOString(),
   },
